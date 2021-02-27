62386bcfb9896850918e44edf8cc220ea8c01b85
Ayman Omer <aymangebrilomer@gmail.com>
2021-02-27 01:10:52 -0500

    useContacts on react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./componets/Home";
+import SecondPage from "./componets/SecondPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <SecondPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/SecondPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SecondPage = () => {
+  return (
+    <div>
+      <h2>here Second Page</h2>
+    </div>
+  );
+};
+
+export default SecondPage;
